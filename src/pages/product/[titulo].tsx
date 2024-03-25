2766a10dd875cddf0c04e12ffd5eb02bf7e4a1a8
Danilo Oliveira <dan.oliveira883@gmail.com>
2024-03-24 22:44:15 -0300

    feat: criada rotas dinamicas para produto que o usuario selecionar, mostrando dados do produto

---
+++ src/pages/product/[titulo].tsx
@@ -0,0 +1,13 @@
+import { InfoProducts } from "@/components/PageFrames/InfoProducts";
+import { NextPage } from "next";
+import { Fragment } from "react";
+
+const ProductPage: NextPage = () => {
+  return (
+    <Fragment>
+      <InfoProducts />
+    </Fragment>
+  );
+};
+
+export default ProductPage;
