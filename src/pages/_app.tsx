87ec0633416f42bf238219015321504fcc4719e5
Danilo Oliveira <dan.oliveira883@gmail.com>
2024-03-24 22:46:06 -0300

    feat: add providers ao redor do componente principal da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
+/* eslint-disable import-helpers/order-imports */
+import { MainTemplate } from "@/template";
 import "../styles/globals.css";
 import "../styles/typography.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { ApiProvider } from "@/context/FetchProductContext";
+import { CartProvider } from "@/context/AddProductCartContext";
+import { ProductSelectionProvider } from "@/context/ProductSelectionContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <ApiProvider>
+        <CartProvider>
+          <ProductSelectionProvider>
+            <MainTemplate>
+              <Component {...pageProps} />
+            </MainTemplate>
+          </ProductSelectionProvider>
+        </CartProvider>
+      </ApiProvider>
     </ThemeProvider>
   );
 }
