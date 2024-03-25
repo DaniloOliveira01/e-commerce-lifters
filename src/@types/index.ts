f62191d01cbbfe0b8ead7568c655300468ddaff2
Danilo Oliveira <dan.oliveira883@gmail.com>
2024-03-24 22:34:04 -0300

    feat: add arquivo para armazenar types globais

---
+++ src/@types/index.ts
@@ -0,0 +1,19 @@
+export interface Color {
+  nome: string;
+  codigo: string;
+}
+
+export interface Photo {
+  url: string;
+  capa: boolean;
+}
+
+export interface Product {
+  titulo: string;
+  valor: string;
+  descricao: string;
+  categoria: string;
+  cores: Color[];
+  tamanhos: string[];
+  fotos: Photo[];
+}
