ca2c2402c3e98e4bd6afe816c8d8268ac3f54689
Danilo Oliveira <dan.oliveira883@gmail.com>
2024-03-26 01:49:33 -0300

    fix: ajustando lógicxa para o banner aparecer apenas na home

--- src/template/index.tsx
+++ src/template/index.tsx
@@ -1,16 +1,19 @@
 import { Banner } from "@/components/Banner";
 import HeaderWeb from "@/components/Header";
-import { Fragment, ReactNode } from "react";
+import { useRouter } from "next/router";
+import { Fragment, ReactNode, useEffect } from "react";
 
 interface IMainTemplateProps {
   children: ReactNode;
 }
 
 export const MainTemplate = ({ children }: IMainTemplateProps) => {
+  const router = useRouter();
+
   return (
     <Fragment>
       <HeaderWeb />
-      <Banner />
+      {router.asPath === "/" && <Banner />}
       <main className="flex pb-10 flex-col min-h-screen overflow-hidden">
         {children}
       </main>
